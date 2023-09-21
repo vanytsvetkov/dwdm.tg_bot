e0d74f16f193878ee29e8d700ca0ea1c2705749b
Ivan Tsvetkov <ivanfromearth@gmail.com>
2023-09-21 14:45:45 +0300

    Added export to build.sh

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ generate_script() {
     local script_content=(
         "#!/bin/bash"
         ""
-        "export PATH=\$PATH:$BASE_DIR"
+        "export PYTHONPATH=$BASE_DIR:\$PYTHONPATH"
         "source $BASE_DIR/venv/bin/activate &&"
         "$BASE_DIR/venv/bin/python3 $BASE_DIR/scripts/$script_name/main.py \"\$@\" > /dev/null 2>&1 &"
       )
@@ -49,7 +49,7 @@ SERVICE=(
   "Type=simple"
   "User=root"
   "WorkingDirectory=$BASE_DIR"
-  "Environment=PATH=\$PATH:$BASE_DIR"
+  "Environment=PYTHONPATH=$BASE_DIR:\$PYTHONPATH"
   "ExecStart=$BASE_DIR/venv/bin/python3 $BASE_DIR/main.py $BASE_DIR/bot.py"
   "Restart=always"
   "RestartSec=10"
